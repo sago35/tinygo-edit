7deb10c114983727b541128e258c12b3e7006154
sago35 <sago35@gmail.com>
2020-09-13 06:24:23 +0900

    Add option to use `tinygo targets`

--- cli.go
+++ cli.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
 	"io"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 
@@ -12,7 +15,16 @@ import (
 
 const (
 	appName        = "tinygo-edit"
-	appDescription = ""
+	appDescription = `This program uses $TINYGOPATH
+  export TINYGOPATH=/path/to/tinygo-root-directory
+
+You can use the following environment variables
+  To get a list of targets from the result of 'tinygo targets':
+    export TINYGO_EDIT_WITH_GOROOT=1
+
+  Using the GOROOT environment variable to link with gopls:
+    export TINYGO_EDIT_WITH_TINYGO_TARGETS=1
+`
 )
 
 type cli struct {
@@ -25,19 +37,28 @@ var (
 	target string
 	editor = app.Flag("editor", "editor path").Default("vim").String()
 	wait   = app.Flag("wait", "wait for the editor to close").Bool()
-	goroot = app.Flag("with-goroot", "use proper GOROOT").Bool()
+	goroot = app.Flag("with-goroot", "use proper GOROOT").Envar("TINYGO_EDIT_WITH_GOROOT").Default("0").Bool()
 )
 
 // Run ...
 func (c *cli) Run(args []string) error {
 	app.UsageWriter(c.errStream)
 
-	if os.Getenv(`TINYGOPATH`) == "" {
-		return fmt.Errorf("$TINYGOPATH is not set. ex: export TINYGOPATH=/path/to/your/tinygo/")
-	}
+	targets := []string{}
+	var err error
 
-	targets, err := getTargets(os.Getenv(`TINYGOPATH`))
-	app.Flag("target", "target name").Default("pyportal").EnumVar(&target, targets...)
+	if _, exists := os.LookupEnv("TINYGO_EDIT_WITH_TINYGO_TARGETS"); exists {
+		targets, err = getTargetsFromTinygoTargets()
+	} else {
+		if os.Getenv(`TINYGOPATH`) == "" {
+			return fmt.Errorf("$TINYGOPATH is not set. ex: export TINYGOPATH=/path/to/your/tinygo/")
+		}
+		targets, err = getTargets(os.Getenv(`TINYGOPATH`))
+	}
+	if err != nil {
+		return err
+	}
+	app.Flag("target", "target name").EnumVar(&target, targets...)
 
 	if VERSION != "" {
 		app.Version(fmt.Sprintf("%s version %s build %s", appName, VERSION, BUILDDATE))
@@ -49,6 +70,11 @@ func (c *cli) Run(args []string) error {
 	k, err := app.Parse(args[1:])
 	if err != nil {
 		return err
+	}
+
+	if target == "" {
+		app.Usage(args[1:])
+		return nil
 	}
 
 	switch k {
@@ -89,4 +115,24 @@ func getTargetsFromJson(tinygopath string) ([]string, error) {
 	}
 
 	return matches, err
+}
+
+func getTargetsFromTinygoTargets() ([]string, error) {
+	buf := new(bytes.Buffer)
+	cmd := exec.Command("tinygo", "targets")
+	cmd.Stdout = buf
+	cmd.Stderr = buf
+
+	err := cmd.Run()
+	if err != nil {
+		return nil, err
+	}
+
+	targets := []string{}
+	scanner := bufio.NewScanner(buf)
+	for scanner.Scan() {
+		targets = append(targets, scanner.Text())
+	}
+
+	return targets, nil
 }
